ef4eeb03688dc40e8a3e9135eb24f63b15658d68
nkkn-prog <git@stackblitz.com>
2024-12-01 16:19:52 +0900

    Initial commit

---
+++ src/components/IncompleteTodos.tsx
@@ -0,0 +1,25 @@
+export const IncompleteTodos = (props) => {
+
+  const {todos, onClickComplete, onClickDelete} = props
+  return (
+    <>
+      <div className="incomplete-area">
+        <p className="title">未完了のTODO</p>
+        <ul>
+          {todos.map((todo, index) => {
+            return (
+              <li key={todo}>
+                <div className="list-row">
+                  <p className="todo-item">{todo}</p>
+                  <button onClick={() => onClickComplete(index)}>完了</button>
+                  {/* 関数の処理の中身として実行したい関数を記載する */}
+                  <button onClick={() => onClickDelete(index)}>削除</button>
+                </div>
+              </li>
+            );
+          })}
+        </ul>
+      </div>
+    </>
+  );
+};
